c5051724679f58a8051d3b91ff99bf9ca3c57ce0
TulinVM <124674817+TulinVM@users.noreply.github.com>
2023-04-10 22:40:00 +0300

    Add files via upload

--- django_d1/project/urls.py
+++ django_d1/project/urls.py
@@ -19,8 +19,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,10 +28,20 @@ urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
     # path('', include('image.urls')),
     # path('', include(('sampleapp.urls'), namespace='sampleapp'))
-
+    # path('users/', include('users.urls')),
+    # path('', include('learning_logs.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# from django.contrib import admin
+# from django.urls import path, include
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('users/', include('users.urls')),
+#     path('', include('learning_logs.urls')),
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Источник: https: // pythonpip.ru / django / zagruzka - izobrazheniya - v - django
 
 # urlpatterns = [
